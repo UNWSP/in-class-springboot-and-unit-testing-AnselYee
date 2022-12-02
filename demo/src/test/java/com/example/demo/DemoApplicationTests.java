7e0e4c5211d5d48335841bae0f936f909a072fb1
Enze Ye <102567414+AnselYee@users.noreply.github.com>
2022-12-01 20:44:56 -0600

    Add files via upload

---
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	void testGetContent() {
+		Greeting greeting= new Greeting(1,"Test");
+		assertEquals("Test",greeting.getContent());
+	}
+}
